a4d962860bff9c0354c8b20bf2a4a98b551410f5
Pachi-Dev <pachi.claros@gmail.com>
2024-04-29 11:46:38 -0600

    Actualizar contenido de la página de inicio en Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
+
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()]
+export default defineConfig({  
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "es",
+      locales: {
+        en: "en",
+        es: "es"
+      }
+    }
+  }), tailwind(), react(), astroI18next()]
 });
---
+++ src/components/ParticulasEffect.jsx
@@ -0,0 +1,144 @@
+import { useEffect, useMemo, useState } from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import { loadSlim } from '@tsparticles/slim'
+
+export function ParticulasEffect() {
+  const [init, setInit] = useState(false)
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine)
+    }).then(() => {
+      setInit(true)
+    })
+  }, [])
+
+  const particlesLoaded = (container) => {
+    console.log(container)
+  }
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: 'transparent',
+        },
+      },
+      fpsLimit: 120,
+      particles: {
+        number: {
+          value: 180,
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: '#ffffff',
+        },
+        shape: {
+          type: 'circle',
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false,
+          },
+        },
+        size: {
+          value: 1,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: '#ff0000',
+          opacity: 0.4,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: 'canvas',
+        events: {
+          onhover: {
+            enable: false,
+            mode: 'repulse',
+          },
+          onclick: {
+            enable: false,
+            mode: 'push',
+          },
+          resize: true,
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      style: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        zIndex: 3,
+      },
+      retina_detect: true,
+    }),
+    []
+  )
+
+  if (init) {
+    return (
+      <Particles
+        id='tsparticles'
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    )
+  }
+
+  return <></>
+}
