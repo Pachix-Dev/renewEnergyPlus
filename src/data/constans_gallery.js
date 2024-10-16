2e811cfea438d15f46e6294675f655f682720c9c
Pachi-Dev <pachi.claros@gmail.com>
2024-10-16 13:33:06 -0600

    cambios re+

---
+++ src/data/constans_gallery.js
@@ -0,0 +1,377 @@
+const gallery2024 = [
+    {
+        src: '/img/gallery/2024/solar2024-1.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-2.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-3.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-4.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-5.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-6.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-7.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-8.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-9.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-10.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-11.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    },
+    {
+        src: '/img/gallery/2024/solar2024-12.webp',
+        width: 1920,
+        height: 1280,
+        alt: 'Solar Storage 2024'
+    }
+]
+
+const gallery2023 = [
+    {
+        src: '/img/gallery/gallerySSM2023-1.jpg',
+        width: 2560,
+        height: 1707,
+        alt: 'Solar Storage 2023'
+    },  
+    {
+        src: '/img/gallery/gallerySSM2023-2.jpg',
+        width: 2560,
+        height: 1707,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-3.jpg',
+        width: 2560,
+        height: 1437,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-4.jpg',
+        width: 2560,
+        height: 1437,
+        alt: 'Solar Storage 2023'
+    }, 
+    {
+        src: '/img/gallery/gallerySSM2023-5.jpg',
+        width: 2560,
+        height: 1437,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-6.jpg',
+        width: 2560,
+        height: 1437,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-7.jpg',
+        width: 2560,
+        height: 1437,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-8.jpg',
+        width: 2560,
+        height: 1600,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-9.jpg',
+        width: 2560,
+        height: 1600,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-10.jpg',
+        width: 2560,
+        height: 1600,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-11.jpg',
+        width: 2560,
+        height: 1600,
+        alt: 'Solar Storage 2023'
+    },
+    {
+        src: '/img/gallery/gallerySSM2023-12.jpg',
+        width: 2560,
+        height: 1600,
+        alt: 'Solar Storage 2023'
+    },
+]
+
+const gallery2022 = [
+    {
+        src: '/img/gallery/gallerySPM2022-1.jpg',
+        width: 1500,
+        height: 1000,
+        alt: 'Solar Storage 2022'
+    },  
+    {
+        src: '/img/gallery/gallerySPM2022-2.jpg',
+        width: 1500,
+        height: 1000,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-3.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-4.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-5.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-6.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-7.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-8.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-9.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-10.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-11.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+    {
+        src: '/img/gallery/gallerySPM2022-12.jpg',
+        width: 1500,
+        height: 844,
+        alt: 'Solar Storage 2022'
+    },
+]
+
+const gallery2021 = [
+    {
+        src: '/img/gallery/gallerySPM2021-1.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    },
+    {
+        src: '/img/gallery/gallerySPM2021-2.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    },
+    {
+        src: '/img/gallery/gallerySPM2021-3.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-4.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-5.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-6.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-7.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-8.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    },
+    {
+        src: '/img/gallery/gallerySPM2021-9.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    },  
+    {
+        src: '/img/gallery/gallerySPM2021-10.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-11.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+    {
+        src: '/img/gallery/gallerySPM2021-12.jpeg',
+        width: 1600,
+        height: 1067,
+        alt: 'Solar Storage 2021'
+    }, 
+]
+
+const gallery2019 = [
+    {
+        src: '/img/gallery/gallerySPM2019-1.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-2.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-3.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-4.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-5.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-6.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-7.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-8.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-9.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-10.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-11.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+    {
+        src: '/img/gallery/gallerySPM2019-12.jpg',
+        width: 1280,
+        height: 720,
+        alt: 'Solar Storage 2019'
+    },
+  ]
+  
+  export { gallery2024, gallery2023, gallery2022, gallery2021, gallery2019 }
+  
