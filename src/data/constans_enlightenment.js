f08e710ef2aacbcb9fe0f89dc8a4eee15981afc5
Pachi-Dev <pachi.claros@gmail.com>
2024-11-26 13:02:22 -0600

    agrega sección de área de iluminación en el menú y actualiza fechas del evento a marzo

---
+++ src/data/constans_enlightenment.js
@@ -0,0 +1,134 @@
+export const conferences = {
+    day_1: [
+        {
+            id: 1,            
+            hour: '12:00 – 12:30',
+            name: 'HITACHI',
+            status: 'VENDIDO',            
+        },
+        {
+            id: 2,
+            hour: '12:45 – 13:15',
+            name: 'METERING',
+            status: 'VENDIDO',
+        },
+        {
+            id: 3,
+            hour: '13:30 – 14:00',
+            name: 'BATTERY DEPOT',
+            status: 'RESERVADO',
+        },
+        {
+            id: 4,
+            hour: '14:00 – 15:00',
+            name: 'BREAK',
+            status: '',
+        },
+        {
+            id: 5,
+            hour: '15:00 – 15:30',
+            name: 'AP SYSTEMS',
+            status: 'VENDIDO',
+        },
+        {
+            id: 6,
+            hour: '15:45 – 16:15',
+            name: 'EXEL SOLAR',
+            status: 'RESERVADO',
+        },
+        {
+            id: 7,
+            hour: '16:30 – 17:00',
+            name: 'LIVOLTEK',
+            status: 'RESERVADO',
+        },
+        {
+            id: 8,
+            hour: '17:15 – 18:00',
+            name: 'DISPONIBLE',
+            status: '$1,500 USD',
+        }
+    ],
+    day_2: [
+        {
+            id: 1,            
+            hour: '12:00 – 12:30',
+            name: 'LONGI',
+            status: 'VENDIDO',            
+        },
+        {
+            id: 2,
+            hour: '12:45 – 13:15',
+            name: 'SOLARMAX',
+            status: 'VENDIDO',
+        },
+        {
+            id: 3,
+            hour: '13:30 – 14:00',
+            name: 'S-5!',
+            status: 'RESERVADO',
+        },
+        {
+            id: 4,
+            hour: '14:00 – 15:00',
+            name: 'BREAK',
+            status: '',
+        },
+        {
+            id: 5,
+            hour: '15:00 – 15:30',
+            name: 'EXEL SOLAR',
+            status: 'RESERVADO',
+        },
+        {
+            id: 6,
+            hour: '15:45 – 16:15',
+            name: 'SOLAREVER',
+            status: 'VENDIDO',
+        },
+        {
+            id: 7,
+            hour: '16:30 – 17:00',
+            name: 'ORKA',
+            status: 'VENDIDO',
+        },
+        {
+            id: 8,
+            hour: '17:15 – 18:00',
+            name: 'DISPONIBLE',
+            status: '$1,500 USD',
+        }
+    ],
+    day_3: [
+        {
+            id: 1,            
+            hour: '12:00 – 12:30',
+            name: 'BATTERY DEPOT',
+            status: 'RESERVADO',            
+        },
+        {
+            id: 2,
+            hour: '12:45 – 13:15',
+            name: 'SOLAREVER',
+            status: 'VENDIDO',
+        },
+        {
+            id: 3,
+            hour: '13:30 – 14:00',
+            name: 'DISPONIBLE',
+            status: '$1,500 USD',
+        },
+        {
+            id: 4,
+            hour: '14:15 – 14:45',
+            name: 'DISPONIBLE',
+            status: '$1,500 USD',
+        },
+        {
+            id: 5,
+            hour: '15:00 – 15:30',
+            name: 'DISPONIBLE',
+            status: '$1,500 USD',
+        },
+    ]
+}
