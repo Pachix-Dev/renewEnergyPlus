6c7d2267846c9895eda1018e357f1f00f7dfcf05
Pachi-Dev <pachi.claros@gmail.com>
2024-12-09 16:36:50 -0600

    refactor: reorganiza el menú y agrega nueva sección para programa premium

---
+++ src/data/constans_program_premium.js
@@ -0,0 +1,23 @@
+export const program_premium = [
+    {
+        title: 'Oportunidades y Beneficios del Abasto Aislado Renovable',
+    },
+    {
+        title: 'Almacenamiento de Energía Eléctrica como detonador de la transición energética',
+    },
+    {
+        title: 'Nuevos modelos de participación en el sector eléctrico de México',
+    },
+    {
+        title: 'Nueva Ley de la Industria Eléctrica: El Cambio de Modelo y sus Implicaciones',
+    },
+    {
+        title: 'Política de Descarbonización 2025-2030'
+    },
+    {
+        title: 'Aumento del umbral de Generación Distribuida: nuevas reglas y normas aplicables',
+    },
+    {
+        title: 'Desafíos asociados a la electromovilidad, la infraestructura de carga y energía limpia'
+    }    
+]
