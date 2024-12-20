6f217fdd6710187bf80fdb37e539db8ff479e7a4
Pachi-Dev <pachi.claros@gmail.com>
2024-12-20 11:32:40 -0600

    actualiza contenido educativo gratuito y mejora la estructura del programa de conferencias

---
+++ src/components/shared/Program.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+export function Program({ conferences, translate, language }) {
+  const [day, setDay] = useState(conferences.day_1)
+  return (
+    <>
+      <div className='text-white flex flex-wrap gap-10 mt-20'>
+        <button
+          onClick={() => setDay(conferences.day_1)}
+          className='bg-[#A9001B] rounded-md hover:bg-[#C64E08] hover:scale-105 ease-in-out duration-300 p-5'
+        >
+          <span class='text-white z-10 font-extrabold text-xl'>
+            {translate.day_1}
+          </span>
+        </button>
+        <button
+          onClick={() => setDay(conferences.day_2)}
+          className='bg-[#A9001B] rounded-md hover:bg-[#C64E08] hover:scale-105 ease-in-out duration-300 p-5'
+        >
+          <span class='text-white z-10 font-extrabold text-xl'>
+            {translate.day_2}
+          </span>
+        </button>
+        <button
+          onClick={() => setDay(conferences.day_3)}
+          className='bg-[#A9001B] rounded-md hover:bg-[#C64E08] hover:scale-105 ease-in-out duration-300 p-5'
+        >
+          <span class='text-white z-10 font-extrabold text-xl'>
+            {translate.day_3}
+          </span>
+        </button>
+      </div>
+      {day.map((conference) => {
+        return (
+          <div className='flex gap-20 text-white my-10 bg-black/50 shadow-md p-6 rounded-md'>
+            <div className='flex gap-2 items-center'>
+              {' '}
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z'
+                />
+              </svg>
+              {conference.hour}
+            </div>
+            <div className=''>
+              <p className='pb-5 text-2xl font-bold'>
+                {language === 'es' ? conference.title : conference.title_en}{' '}
+              </p>
+              <span className='bg-green-700 p-1 rounded-md italic'>
+                {language === 'es'
+                  ? conference.tag
+                  : conference?.tag_en | conference.tag}
+              </span>
+            </div>
+          </div>
+        )
+      })}
+    </>
+  )
+}
--- src/data/constans_program_premium.js
+++ src/data/constans_program_premium.js
@@ -1,23 +1,69 @@
-export const program_premium = [
-    {
-        title: 'Oportunidades y Beneficios del Abasto Aislado Renovable',
-    },
-    {
-        title: 'Almacenamiento de Energía Eléctrica como detonador de la transición energética',
-    },
-    {
-        title: 'Nuevos modelos de participación en el sector eléctrico de México',
-    },
-    {
-        title: 'Nueva Ley de la Industria Eléctrica: El Cambio de Modelo y sus Implicaciones',
-    },
-    {
-        title: 'Política de Descarbonización 2025-2030'
-    },
-    {
-        title: 'Aumento del umbral de Generación Distribuida: nuevas reglas y normas aplicables',
-    },
-    {
-        title: 'Desafíos asociados a la electromovilidad, la infraestructura de carga y energía limpia'
-    }    
-]
+export const program_premium = {
+    day_1: [
+        {
+            hour: '12:00 - 12:30',
+            title: 'Plan Nacional de Energía',
+            title_en: 'National Energy Plan',
+            tag: 'Conferencia',
+            tag_en: 'Conference',
+            color:'#1F2937'
+        },
+        {
+            hour: '12:45 - 13:45',
+            title: 'Nueva Ley de la Industria Eléctrica: El Cambio de Modelo y sus Implicaciones',
+            tag: 'Panel especial',
+            tag_en: 'Special Panel',
+            color: '#0183FD'
+        },
+        {
+            hour: '14:00 - 14:45',
+            title: 'Oportunidades y Beneficios del Abasto Aislado Renovable',
+            tag: 'Panel', 
+            color: '#016A02'       
+        },        
+    ],
+    day_2:[
+        {
+            hour: '10:00 - 11:00',
+            title: 'Nuevos modelos de participación en el sector eléctrico de México',
+            tag: 'Panel especial',
+            tag_en: 'Special Panel',
+            color: '#0183FD'
+        }, 
+        {
+            hour: '11:00 - 11:45',
+            title: 'Plan de Energía del estado de Jalisco',
+            tag: 'Conferencia',
+            tag_en: 'Conference',
+            color:'#1F2937'
+        }, 
+        {
+            hour: '12:00 - 12:45',
+            title: 'Economía Circular',
+            tag: 'Panel',
+            color: '#016A02'
+
+        },
+        {
+            hour: '14:00 - 14:45',
+            title: 'Ventanilla Única: alcance de la simplificación administrativa',
+            tag: 'Panel',
+            color: '#016A02'    
+        }  
+    ],
+    day_3:[
+        {
+            hour: '10:00 - 11:00',
+            title: 'Almacenamiento de Energía Eléctrica como detonador de la transición energética',
+            tag: 'Panel especial',
+            tag_en: 'Special Panel',
+            color: '#0183FD'
+        },
+        {
+            hour: '13:00 - 13:45',
+            title: 'Aumento del umbral de Generación Distribuida: nuevas reglas y normas aplicables',
+            tag: 'Panel',
+            color: '#016A02'
+        } 
+    ]
+};
