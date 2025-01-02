5193cb89f7ee1f3abe29d51874d7bf1a9c176d50
Pachi-Dev <pachi.claros@gmail.com>
2025-01-02 13:50:11 -0600

    añade URL de notificación en la creación de pedidos

--- server/app.js
+++ server/app.js
@@ -99,7 +99,8 @@ app.post('/create-order-replus', async (req, res) => {
                     first_name: body?.name,
                     last_name: body?.paternSurname,
                     email: body?.paymentData.payer.email },
-            statement_descriptor: "REPLUSMEXICO2025",            
+            statement_descriptor: "REPLUSMEXICO2025",
+            notification_url: "https://re-plus-mexico.com.mx/server/webhook-mp",
             external_reference: body.uuid,
             installments: 1,
         };
