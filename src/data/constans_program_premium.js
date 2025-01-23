3bef5793ced112d029fd8ec5bd79ea5f106db289
IDGS802-21000115 <daianaulloa8@gmail.com>
2025-01-23 14:52:17 -0600

    cambio a variables del las conferencias premium

--- src/data/constans_program_premium.js
+++ src/data/constans_program_premium.js
@@ -2,19 +2,49 @@ export const program_premium = {
     day_1: [
         {
             hour: '12:00 - 12:30',
-            title: 'Plan Nacional de Energía',
-            title_en: 'National Energy Plan',
+            title: 'Estrategia Nacional del Sector Eléctrico',
+            title_en: 'National Electricity Sector Strategy',
             tag: 'Conferencia',
             tag_en: 'Conference',
-            color:'#1F2937'
+            color:'#1F2937',
+            resume: '¿Cuáles son los alcances de la nueva política energética en materia de electricidad? ¿Cuál es el nuevo rol de la planeación? ¿Cómo se propone impulsar la transición energética?',
+            resume_en: 'What is the scope of the new energy policy on electricity? What is the new role of planning? How is it proposed to promote the energy transition?',    
+            speakers: [
+                {       
+                    id: 1,  
+                    imagenes:'',              
+                    name: 'Representante de la Subsecretaría de Planeación y Transición Energética de la Secretaría de Energía (no confirmado)',
+                    bio: '',
+                    bio_en: '',
+                    position: '',
+                    email: '',
+                    phone: '',
+                   
+                },                
+            ],     
         },
         {
             hour: '12:45 - 13:45',
-            title: 'Nueva Ley de la Industria Eléctrica: El Cambio de Modelo y sus Implicaciones',
-            title_en: 'New Electricity Industry Law: Model Change and Implications',
+            title: 'Nueva legislación en materia de electricidad',
+            title_en: 'Nueva legislación en materia de electricidad',
             tag: 'Panel especial',
             tag_en: 'Special Panel',
-            color: '#0183FD'
+            color: '#0183FD',
+            resume: 'Después de la reforma a la Constitución Política en materia de electricidad se emitirán leyes secundarias que regulen la operación y organización del sector ¿Qué ajustes podemos esperar? ¿Cuál será el papel de la CFE? ¿Cómo podrá participar como iniciativa privada?',
+            resume_en: 'After the reform of the Constitution regarding electricity, secondary laws will be issued to regulate the operation and organization of the sector. What adjustments can we expect? What will be the role of the CFE? How will it be able to participate as a private initiative?',    
+            speakers: [
+                {       
+                    id: 1,  
+                    imagenes:'/img/José María Lujambio.webp',              
+                    name: 'José María Lujambio',
+                    bio: 'Profesional con amplia experiencia en el campo de generación de energía con sistemas fotovoltáicos. Contribuye actualmente y de manera clave al lanzamiento de varias iniciativas y soluciones técnicas en equipos de medición y evaluación de eficiencia en Paneles Fotovoltáicos. Actualmente trabaja en el desarrollo comercial de la línea de productos renovables de SEAWARD para el mercado norteamericano. ',
+                    bio_en: 'Professional with extensive experience in the field of power generation with photovoltaic systems. He is currently a key contributor to the launching of several initiatives and technical solutions in measurement equipment and evaluation of efficiency in Photovoltaic Panels. He is currently working on the commercial development of SEAWARD´s renewable product line for the North American market. ',
+                    position: 'Business Development Manager, SEAWARD Solar Products Norteamérica',
+                    email: 'jademiguel@metering.com.mx',
+                    phone: '55 45 69 41 39',
+                   
+                },                
+            ],    
         },
         {
             hour: '14:00 - 14:45',
@@ -22,7 +52,22 @@ export const program_premium = {
             title_en: 'Opportunities and Benefits of Isolated Renewable Supply',
             tag: 'Panel', 
             tag_en: 'Panel',
-            color: '#016A02'       
+            color: '#016A02' ,
+            resume: 'Ante la inminente publicación de nueva regulación en el sector eléctrico, el Abasto Aislado se posiciona como una opción clave para el desarrollo de proyectos de energía renovable ¿Cuál es el alcance que se espera de las nuevas reglas para el Abasto Aislado renovable y cómo pueden permitir impulsar aún más esta figura?',
+            resume_en: 'With the imminent publication of new regulations in the electricity sector, Isolated Supply is positioned as a key option for the development of renewable energy projects. What is the expected scope of the new rules for renewable Isolated Supply and how can they further promote this figure?',    
+            speakers: [
+                {       
+                    id: 1,  
+                    imagenes:'',              
+                    name: '',
+                    bio: '',
+                    bio_en: '',
+                    position: '',
+                    email: '',
+                    phone: '',
+                   
+                },                
+            ],          
         },        
     ],
     day_2:[
