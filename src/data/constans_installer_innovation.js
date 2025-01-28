0bd9e3b25629baf99ee649b2620aa9b1c1d08c30
IDGS802-21000115 <daianaulloa8@gmail.com>
2025-01-28 14:37:47 -0600

    Cambios de diseño

--- src/data/constans_installer_innovation.js
+++ src/data/constans_installer_innovation.js
@@ -1,21 +1,59 @@
 export const installerInnovation = {
     day_1: [
+        
         {
             id: 1,
-            hour: '11:30-14:30',
-            name: 'Navigating the Future of Energy Storage in Mexico: Opportunities and Monetization Strategies for 2025',
-            descripction: 'Únase a un taller de inmersión que desentraña el dinámico mercado de almacenamiento de energía en México, ofreciendo una perspectiva de futuro adaptada para 2025. Esta sesión está diseñada para atraer a una audiencia diversa, incluyendo instaladores, desarrolladores, proveedores de servicios públicos, innovadores tecnológicos, legisladores y actores clave dentro del ecosistema de almacenamiento de energía. Los participantes explorarán en profundidad las oportunidades en el almacenamiento de energía, desde aplicaciones residenciales y comerciales hasta despliegues a escala de servicios públicos, alineándose con el panorama energético en evolución de México. El taller destacará los avances en las tecnologías de almacenamiento, las vías de monetización procesables y los marcos regulatorios que dan forma a la industria. Obtenga información exclusiva de las principales organizaciones de investigación y aprenda a través de estudios de casos prácticos presentados por destacados expertos de la industria. Descubra cómo el almacenamiento de energía puede impulsar la resiliencia de la red, permitir la integración renovable y ofrecer un valor tangible en todos los sectores, capacitando a los participantes para aprovechar todo el potencial de este mercado transformador en la transición energética de México.',
-            description_en: 'Join an immersive workshop that unpacks the dynamic energy storage market in Mexico, offering a forward-looking perspective tailored for 2025. This session is crafted to engage a diverse audience, including installers, developers, utility providers, technology innovators, policymakers, and key stakeholders within the energy storage ecosystem. Participants will explore in-depth opportunities in energy storage, from residential and commercial applications to utility-scale deployments, aligning with Mexico´s evolving energy landscape. The workshop will highlight advancements in storage technologies, actionable monetization pathways, and the regulatory frameworks shaping the industry. Gain exclusive insights from leading research organizations and learn through practical case studies presented by prominent industry experts. Discover how energy storage can drive grid resilience, enable renewable integration, and deliver tangible value across sectors, empowering participants to harness the full potential of this transformative market in Mexico´s energy transition.',  
-        }
+            hour: '11:30 – 13:30',
+            name: '',
+            status: 'VENDIDO',
+            duracion:'30 min.',
+            logos:'/img/sponsors/APsystemss.webp',
+            title:' Navegando el Futuro del Amacenamiento de Energía en México: Oportunidades y Estrategias de Monetización para 2025.',
+            title_en:'Navigating the Future of Energy Storage in Mexico: Opportunities and Monetization Strategies for 2025',
+            resume:'Únase a un taller inmersivo que desempeña el dinámico mercado de almacenamiento de energía en México, ofreciendo una perspectiva orientada al futuro y adaptada para 2025. Esta sesión está diseñada para involucrar a una audiencia diversa, incluidos instaladores, desarrolladores, proveedores de servicios públicos, innovadores tecnológicos, legisladores y actores clave dentro del ecosistema de almacenamiento de energía.Los participantes explorarán a fondo las oportunidades en el almacenamiento de energía, desde aplicaciones residenciales y comerciales hasta implementaciones a escala de servicios públicos, alineándose con el panorama energético en evolución de México. El taller destacará los avances en tecnologías de almacenamiento, caminos prácticos de monetización y los marcos regulatorios que están moldeando la industria. Obtenga información exclusiva de organizaciones líderes en investigación y aprenda a través de casos prácticos presentados por destacados expertos del sector.Descubra cómo el almacenamiento de energía puede impulsar la resiliencia de la red, facilitar la integración de energías renovables y generar un valor tangible en diversos sectores, empoderando a los participantes para aprovechar al máximo el potencial de este mercado transformador en la transición energética de México.',
+            resume_en:'Join an immersive workshop that unpacks the dynamic energy storage market in Mexico, offering a forward-looking perspective tailored for 2025. This session is crafted to engage a diverse audience, including installers, developers, utility providers, technology innovators, policymakers, and key stakeholders within the energy storage ecosystem. Participants will explore in-depth opportunities in energy storage, from residential and commercial applications to utility-scale deployments, aligning with Mexico evolving energy landscape. The workshop will highlight advancements in storage technologies, actionable monetization pathways, and the regulatory frameworks shaping the industry. Gain exclusive insights from leading research organizations and learn through practical case studies presented by prominent industry experts. Discover how energy storage can drive grid resilience, enable renewable integration, and deliver tangible value across sectors, empowering participants to harness the full potential of this transformative market in Mexico energy transition.',
+            speakers: [
+                {       
+                    id: 1,  
+                    nameem: 'AP SYSTEMS',              
+                    name: 'José Cataño',
+                    bio: 'El Ing. Cataño ha tenido una carrera técnica desde pequeño durante su preparatoria, posteriormente se graduó como Ingeniero en Mantenimiento Industrial en la Universidad Tecnológica de Jalisco. En 2019 comenzó en el área de sistemas con la empresa Gen-t para Herbalife Nutrition, después apoyó como consultor externo para el ERP Oracle. Ha tenido experiencia en apps de negocio, administración de negocios, base de datos y administración de base de datos usando Power BI.',
+                    bio_en: 'Mr. Cataño has had a technical career since childhood during his high school, later graduated as an Engineer in Industrial Maintenance from the Technological University of Jalisco. In 2019 he started in the area of systems with the company Gen-t for Herbalife Nutrition, then supported as an external consultant for the ERP Oracle. She has had experience in business apps, business management, database and database management using Power BI.',
+                    position_en: 'Technical Support Specialist',
+                    position:'especialista de soporte técnico',
+                    email: 'jose.catano@apsystems.com',
+                    phone: '52 33 26179699',
+                    imagenes:'/img/5mar-JoséCataño-APSystem.png',
+                },                
+            ],  
+        },
     ],
     day_2: [
         {
-            id: 2,
-            hour: '11:30-14:30',
-            name: 'Masterclass de Almacenamiento de Energía 2025: Estrategias empresariales transformadoras, tecnologías avanzadas para México',
-            name_en: 'Energy Storage Masterclass 2025: Transformative Business Strategies, Advanced Technologies for Mexico',
-            descripction: 'Mejore sus conocimientos y habilidades en almacenamiento de energía con un taller especializado diseñado para el mercado energético de México, en rápida evolución. La «Masterclass de Almacenamiento de Energía 2025: Estrategias de negocio transformadoras, tecnologías avanzadas y sistemas inteligentes» está diseñado para profesionales que se enfrentan a los retos y oportunidades del almacenamiento de energía en los sectores residencial, comercial y de servicios públicos. Este taller profundiza en modelos de negocio innovadores, tecnologías de almacenamiento de última generación y diseño de sistemas inteligentes, centrándose en aplicaciones prácticas relevantes para el panorama energético de México. Aprenda a través de estudios de casos reales de inmersión y conocimientos de los principales expertos acerca de cómo el almacenamiento de energía impulsa la optimización de la red, impulsa la integración de las energías renovables, y apoya los esfuerzos de descarbonización. Descubra diversas configuraciones de almacenamiento, estrategias operativas e impactos ambientales, que le permitirán mantenerse a la vanguardia en el competitivo y transformador mercado del almacenamiento de energía para 2025 en México.',
-            description_en: 'Enhance your knowledge and skills in energy storage with a specialized workshop designed for Mexico’s rapidly evolving energy market. The "Energy Storage Masterclass 2025: Transformative Business Strategies, Advanced Technologies, and Intelligent Systems" is tailored for professionals navigating the challenges and opportunities in energy storage across residential, commercial, and utility sectors. This workshop delves into innovative business models, state-of-the-art storage technologies, and intelligent system design, focusing on practical applications relevant to Mexico´s energy landscape. Learn through immersive real-life case studies and insights from leading experts about how energy storage drives grid optimization, boosts renewable energy integration, and supports decarbonization efforts. Discover diverse storage configurations, operational strategies, and environmental impacts, empowering you to stay ahead in the competitive and transformative energy storage market for 2025 in Mexico.',
-        }
+            id: 1,
+            hour: '15:00 – 15:30',
+            name: 'AP SYSTEMS',
+            status: 'VENDIDO',
+            duracion:'30 min.',
+            logos:'/img/sponsors/APsystemss.webp',
+            title:'Tendencias en el mercado Latinoaméricano para tecnología de conversión energética',
+            title_en:'Latin American Market Trends for Energy Conversion Technology',
+            resume:'APsystems presenta las nuevas tendencias en tecnología MLPE y almacenamiento de energía. Año con año la tecnología APsystems desarrolla nuevas soluciones creadas especialmente para el mercado de México y Latinoamérica. Conoce el nuevo microinversor DS3-LV y una solución más potente para el Power Conversion Systems de APstorage.',
+            resume_en:'APsystems presents new trends in MLPE technology and energy storage. Year after year APsystems technology develops new solutions created especially for the Mexican and Latin American market. Meet the new DS3-LV microinverter and a more powerful solution for APstorage Power Conversion Systems.',
+            speakers: [
+                {       
+                    id: 1,  
+                    nameem: 'AP SYSTEMS',              
+                    name: 'José Cataño',
+                    bio: 'El Ing. Cataño ha tenido una carrera técnica desde pequeño durante su preparatoria, posteriormente se graduó como Ingeniero en Mantenimiento Industrial en la Universidad Tecnológica de Jalisco. En 2019 comenzó en el área de sistemas con la empresa Gen-t para Herbalife Nutrition, después apoyó como consultor externo para el ERP Oracle. Ha tenido experiencia en apps de negocio, administración de negocios, base de datos y administración de base de datos usando Power BI.',
+                    bio_en: 'Mr. Cataño has had a technical career since childhood during his high school, later graduated as an Engineer in Industrial Maintenance from the Technological University of Jalisco. In 2019 he started in the area of systems with the company Gen-t for Herbalife Nutrition, then supported as an external consultant for the ERP Oracle. She has had experience in business apps, business management, database and database management using Power BI.',
+                    position_en: 'Technical Support Specialist',
+                    position:'especialista de soporte técnico',
+                    email: 'jose.catano@apsystems.com',
+                    phone: '52 33 26179699',
+                    imagenes:'/img/5mar-JoséCataño-APSystem.png',
+                },                
+            ],  
+        },
     ]
 };
