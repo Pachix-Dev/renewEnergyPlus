8a5cfad28203ee4aca55b271259f0ef88fc1d66d
BravoDario <adriandariobravo@gmail.com>
2025-02-27 11:24:15 -0600

    new banner for Directorio-digital

--- src/components/directorio-digital/ListDirectory.jsx
+++ src/components/directorio-digital/ListDirectory.jsx
@@ -88,66 +88,71 @@ const ListDirectory = ({ currentLanguage }) => {
     }
 
     return (
-        <div className="z-0 px-2">
-            <div className="z-0 px-2">
-                <div className="w-full my-2">
-                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">
-                        Search
-                    </label>
-                    <div className="relative md:mx-20">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </div>
-                        <input
-                            type="search"
-                            id="default-search"
-                            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-0 focus:border-[#bc0100]"
-                            placeholder={currentLanguage === "en" ? "Search exhibitor" : "Buscar exhibidor"}
-                            value={filter}
-                            onChange={handleFilter}
-                        />
-                        <button
-                            type="submit"
-                            className="absolute end-2.5 bottom-2.5 bg-gradient-to-r from-[#bc0100] to-[#d86a03] text-white hover:brightness-[105%] focus:ring-2 focus:outline-none focus:ring-[#d86a03] font-medium rounded-lg text-sm px-4 py-2"
-                        >
-                            {currentLanguage === "en" ? "Search" : "Buscar"}
-                        </button>
+        <div className="flex flex-col gap-4">
+            <div className="w-full my-2">
+                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">
+                    Search
+                </label>
+                <div className="relative md:mx-20">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
                     </div>
-                </div>
-
-                {/* Pagination handlers */}
-                <div className="flex flex-col align-middle items-center gap-2">
-                    <div className="isolate inline-flex -space-x-px rounded-md shadow-xs">
-                        <button
-                            type="button"
-                            className="bg-gradient-to-l from-[#bc0100] to-[#d86a03] text-white relative inline-flex items-center rounded-l-md px-2 py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            onClick={() => previousPage()}
-                        >
-                            <span className="sr-only">Previous</span>
-                            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                        {createpageButtons()}
-                        <button
-                            type="button"
-                            className="bg-gradient-to-r from-[#bc0100] to-[#d86a03] text-white relative inline-flex items-center rounded-l-md px-2 py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 rotate-180"
-                            onClick={nextPage}
-                        >
-                            <span className="sr-only">Previous</span>
-                            <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-                            </svg>
-                        </button>
-                    </div>
+                    <input
+                        type="search"
+                        id="default-search"
+                        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-0 focus:border-[#bc0100]"
+                        placeholder={currentLanguage === "en" ? "Search exhibitor" : "Buscar expositor"}
+                        value={filter}
+                        onChange={handleFilter}
+                    />
+                    <button
+                        type="submit"
+                        className="absolute end-2.5 bottom-2.5 bg-gradient-to-r from-[#bc0100] to-[#d86a03] text-white hover:brightness-[105%] focus:ring-2 focus:outline-none focus:ring-[#d86a03] font-medium rounded-lg text-sm px-4 py-2"
+                    >
+                        {currentLanguage === "en" ? "Search" : "Buscar"}
+                    </button>
                 </div>
             </div>
+
+            <div className="w-full h-32">
+                <a href="https://mx.ecoflow.com/" target="_blank" className="bg-slate-950">
+                    <img className="h-full mx-auto" src="/img/banners/ecoflow_banner_directorio.webp" alt="EcoFlow Banner" srcset="" />
+                </a>
+            </div>
+
+            {/* Pagination handlers */}
+            <div className="flex flex-col align-middle items-center gap-2">
+                <div className="isolate inline-flex -space-x-px rounded-md shadow-xs">
+                    <button
+                        type="button"
+                        className="bg-gradient-to-l from-[#bc0100] to-[#d86a03] text-white relative inline-flex items-center rounded-l-md px-2 py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                        onClick={() => previousPage()}
+                    >
+                        <span className="sr-only">Previous</span>
+                        <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                    {createpageButtons()}
+                    <button
+                        type="button"
+                        className="bg-gradient-to-r from-[#d86a03] to-[#bc0100] text-white relative inline-flex items-center rounded-r-md px-2 py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                        onClick={nextPage}
+                    >
+                        <span className="sr-only">Previous</span>
+                        <svg className="size-5  rotate-180" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
             {directorioDigital.length > 0 ?
                 directorioDigital.slice((currentPage - 1) * maxSize, currentPage * maxSize).map((item, index) => {
                     return (
-                        <div className="bg-gray-500 text-white flex flex-col md:flex-row align-middle items-center gap-4 my-4 mx-auto rounded-lg p-2 w-full md:w-11/12" key={index}>
+                        <div className="bg-gray-500 text-white flex flex-col md:flex-row align-middle items-center gap-4 mx-auto rounded-lg p-2 w-full md:w-11/12" key={index}>
                             <div className="rounded-xl z-10 bg-gray-300 w-3/5 md:w-1/4 border-2 border-slate-400 p-2">
                                 <img
                                     src={item.imagen.src}
@@ -317,6 +322,35 @@ const ListDirectory = ({ currentLanguage }) => {
                     No hay resultados
                 </h1>
             }
+
+            
+
+            {/* Pagination handlers */}
+            <div className="flex flex-col align-middle items-center gap-2 mb-8">
+                <div className="isolate inline-flex -space-x-px rounded-md shadow-xs">
+                    <button
+                        type="button"
+                        className="bg-gradient-to-l from-[#bc0100] to-[#d86a03] text-white relative inline-flex items-center rounded-l-md px-2 py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                        onClick={() => previousPage()}
+                    >
+                        <span className="sr-only">Previous</span>
+                        <svg className="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                    {createpageButtons()}
+                    <button
+                        type="button"
+                        className="bg-gradient-to-r from-[#d86a03] to-[#bc0100] text-white relative inline-flex items-center rounded-r-md px-2 py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                        onClick={nextPage}
+                    >
+                        <span className="sr-only">Previous</span>
+                        <svg className="size-5  rotate-180" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
