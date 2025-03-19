da0e16648d640b827b9400be60ff6265cad675b3
Pachi-Dev <pachi.claros@gmail.com>
2025-03-18 18:10:58 -0600

    Agregar imagen de stand equipado y actualizar textos y enlaces en varias páginas para RE+ México 2025

--- src/components/home/CounterDown.jsx
+++ src/components/home/CounterDown.jsx
@@ -2,7 +2,7 @@ import { useCountdown } from '../../hooks/useCounterDown.js'
 
 export function CounterDown({ dias, horas, minutos, segundos }) {
   const { days, hours, minutes, seconds } = useCountdown(
-    '5 March 2025 09:00:00'
+    '14 April 2026 09:00:00'
   )
   return (
     <div className='text-white bg-transparent bg-opacity-50 backdrop-blur-xl flex text-center md:grid md:grid-cols-2 md:text-left lg:flex lg:text-center justify-center gap-6 text-md md:text-3xl font-semibold py-5 px-0 sm:px-5 md:px-8 lg:px-2 xl:px-8 border rounded-lg'>
